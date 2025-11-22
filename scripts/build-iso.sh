48161e0e4bed77ab171cf33cc246a51759571e89
Toni Tienda <tonitienda@gmail.com>
2025-11-22 20:03:40 +0100

    Allow build-iso.sh to proceed without exiting
    Remove the forced exit to allow the build process to continue.

--- scripts/build-iso.sh
+++ scripts/build-iso.sh
@@ -5,5 +5,3 @@ echo "[build-iso] TODO: build EnzOS ISO"
 # You will later:
 #   - compile bootloader + kernel
 #   - run grub-mkrescue or similar
-# For now, just fail so you notice when it runs:
-exit 1
