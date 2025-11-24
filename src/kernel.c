7948f08b4c0caa386ec40da12a6404a4c86180fc
Toni Tienda <tonitienda@gmail.com>
2025-11-24 08:31:54 +0100

    Update boot message in kernel initialization

--- src/kernel.c
+++ src/kernel.c
@@ -110,6 +110,5 @@ void kernel_main(void)
 	/* Initialize terminal interface */
 	terminal_initialize();
 
-	/* Newline support is left as an exercise. */
-	terminal_writestring("Hello, kernel World!\n");
+	terminal_writestring("EnzOS booted successfully.\n");
 }
