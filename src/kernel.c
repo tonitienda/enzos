ef5e67e0a0bfcf1866f9cd24100356fc6e45a43f
Toni Tienda <tonitienda@gmail.com>
2025-12-01 22:00:07 +0100

    add padding on top

--- src/kernel.c
+++ src/kernel.c
@@ -116,7 +116,7 @@ void kernel_main(void)
 {
 	/* Initialize terminal interface */
 	terminal_initialize();
-	                         
+	terminal_writestring("\n");	                         
 	terminal_writestring("▗▄▄▄▖           ▗▄▖  ▗▄▖ \n");
 	terminal_writestring("▐▛▀▀▘           █▀█ ▗▛▀▜ \n");
 	terminal_writestring("▐▌   ▐▙██▖▐███▌▐▌ ▐▌▐▙   \n");
